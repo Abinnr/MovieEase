34ff4ecd4dce31414b095f2d3df74b17cff0a05b
abin <abinnr12345@gmail.com>
2025-07-26 22:12:44 +0530

    navigation bar & banner set with content-box

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Banner.css';
+
+const Banner = () => {
+  return (
+    <div className='banner'>
+        <div className='content'>
+            <h1 className='title'>Welcome to Natflix</h1>
+            <div className='banner-buttons'>
+                <button className='button'>Play</button>
+                <button className='button'>My List</button>
+            </div>
+            <h1 className='description'>lalalalalalalalalllalalallalalalala</h1>
+        </div>
+        <div className='fade-bottom'></div>
+    </div>
+  )
+}
+
+export default Banner
