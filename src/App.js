7968935e2f47eee42e3a4bb71c5edfd2d11b0a5b
abin <abinnr12345@gmail.com>
2025-07-31 16:54:17 +0530

    added props to reuse rowpost

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import {originals, action,comedy} from './urls';
 import NavBar from './Components/NavBar/NavBar';
 import Banner from './Components/Banner/Banner';
 import RowPost from './Components/RowPost/RowPost';
-
+// import RowPost2 from './Components/RowPost/RowPost2';
 
 
 function App() {
   return (
-    <div className="Natflix App">
+    <div className="Netflix App">
       <NavBar />
       <Banner />
-      <RowPost />
+      <RowPost title='Netflix Originals' url={originals} />
+      <RowPost title='Actions' isSmall url={action} />
+      <RowPost title='Comedy' isSmall url={comedy} />
     </div>
   );
 }
--- src/Components/RowPost/RowPost2.js
+++ src/Components/RowPost/RowPost2.js
@@ -1,33 +1,29 @@
 import React,{useEffect,useState} from 'react'
-import './RowPost.css';
-import { imageUrl } from '../../Constants/Constants';
+import './RowPost2.css';
+import { API_KEY ,comedyUrl } from '../../Constants/Constants';
 import axios from '../../axios'
-const RowPost = (props) => {
+const RowPost = () => {
   const [movies,setMovies]=useState([])
   useEffect(()=>{
-    axios.get(props.urls).then(responce=>{
+    axios.get(`discover/movie?api_key=${API_KEY}&with_genres=35`).then(responce=>{
       console.log(responce.data)
       setMovies(responce.data.results)
     }).catch(err=>{
       //alert('Network error')
     })
-    
-
-
   },[])
   return (
     <>
+
     <div className='row'>
-        <h2>{props.title}</h2>
+        <h2>Featured</h2>
         <div className='posters'>
           {movies.map((obj)=>
-          <img src={`${imageUrl+obj.backdrop_path}`}alt='MovieEase movies' className={props.isSmall?'smallpost1':'post1'} />
+          <img src={`${comedyUrl+obj.backdrop_path}`}alt='Comedy' className='post1' />
           )}
 
         </div>
     </div>
-
-    
     </>
     
 
