e1c4a2a4dc373dc8d7c1ccb562c4bf4575625e6b
Abinnr <abinnr12345@gmail.com>
2025-08-01 11:30:19 +0530

    Added more genre. Aded demo youtube playbutton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import {originals, action,comedy} from './urls';
+import {originals, action,comedy,horror,romance,documentary} from './urls';
 import NavBar from './Components/NavBar/NavBar';
 import Banner from './Components/Banner/Banner';
 import RowPost from './Components/RowPost/RowPost';
@@ -10,12 +10,16 @@ import RowPost from './Components/RowPost/RowPost';
 
 function App() {
   return (
-    <div className="Netflix App">
+    <div className="MovieEase App">
       <NavBar />
       <Banner />
-      <RowPost title='Netflix Originals' url={originals} />
+      <RowPost title='MovieEase Originals' url={originals} />
       <RowPost title='Actions' isSmall url={action} />
       <RowPost title='Comedy' isSmall url={comedy} />
+      <RowPost title='Horror' isSmall url={horror} />
+      <RowPost title='Romance' isSmall url={romance} />
+      <RowPost title='Documentaries ' isSmall url={documentary} />
+
     </div>
   );
 }
--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,30 +1,46 @@
 import React,{useEffect,useState} from 'react'
+import YouTube from 'react-youtube';
 import './RowPost.css';
 import { imageUrl } from '../../Constants/Constants';
 import axios from '../../axios'
 const RowPost = (props) => {
   const [movies,setMovies]=useState([])
   useEffect(()=>{
-    axios.get(props.urls).then(responce=>{
+    axios.get(props.url).then(responce=>{
       console.log(responce.data)
       setMovies(responce.data.results)
     }).catch(err=>{
       //alert('Network error')
     })
-    
-
-
   },[])
+
+
+  const [urlid,setUrlId]=useState('')
+  const opts = {
+      height: '390',
+      width: '100%',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+      },
+    };
+
+    const handleMovie=(id)=>{
+      console.log(id)
+      axios.get()
+    }
   return (
     <>
     <div className='row'>
         <h2>{props.title}</h2>
         <div className='posters'>
           {movies.map((obj)=>
-          <img src={`${imageUrl+obj.backdrop_path}`}alt='MovieEase movies' className={props.isSmall?'smallpost1':'post1'} />
+          <img onClick={()=>handleMovie(obj.id)} src={`${imageUrl+obj.backdrop_path}`}alt='MovieEase movies' className={props.isSmall?'smallpost1':'post1'} />
           )}
 
         </div>
+
+        <YouTube videoId="2g811Eo7K8U" opts={opts}  />
     </div>
 
     
